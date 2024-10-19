bb6b45f2117265be7f21f7ffbef3205ad2c3931b
Anshu Kumar <112246594+anshu208@users.noreply.github.com>
2024-10-19 20:02:05 +0530

    Add files via upload

---
+++ OOps.java
@@ -0,0 +1,9 @@
+public class OOps {
+    public static void main(String[] args) {
+        int a ;
+
+        for (int i= 1 ; i<100; i++){
+            System.out.print("hello");
+        }
+    }
+}
